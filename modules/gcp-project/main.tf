61df1139b63e8f5c5c1dadc54ccd083f3278424f
ankushjha-aj <jhaankushcpp@gmail.com>
2026-06-02 16:48:51 +0530

    Bucket Module Added

--- modules/gcp-project/main.tf
+++ modules/gcp-project/main.tf
@@ -6,6 +6,7 @@ resource "google_project" "project" {
   org_id          = var.org_id != "" ? var.org_id : null
   folder_id       = var.folder_id != "" ? var.folder_id : null
   billing_account = var.billing_account != "" ? var.billing_account : null
+  deletion_policy = "DELETE"
 
   lifecycle {
     prevent_destroy = false
